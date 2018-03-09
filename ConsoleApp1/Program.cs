be24a87ad0acd3de8e96667af5fda23236cf83fb
admin <admin@PM11-20>
2018-03-09 14:58:46 +0700

    fjkgjkfgjksfjkgfsjkgf

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine("Bai 1");
+            String hostName = Dns.GetHostName();
+            Console.WriteLine("Ten may: " + hostName);
+            IPHostEntry ip = Dns.GetHostEntry(hostName);
+            int ipNo = 0;
+            foreach (IPAddress ipAdd in ip.AddressList)
+                Console.WriteLine("IP thu " + ++ipNo + " la: " + ipAdd.ToString());
+            Console.WriteLine();
+
+            //2
+            Console.WriteLine("Bai 2");
+            Console.WriteLine("Nhap IP hoac ten mien: ");
+            string host = Console.ReadLine();
+            IPHostEntry domainName = Dns.GetHostEntry(host);
+            Console.WriteLine("Ten mien: " + domainName.HostName);
+            foreach (string s in domainName.Aliases)
+                Console.WriteLine("Bi danh: " + s);
+            foreach (IPAddress i in domainName.AddressList)
+                Console.WriteLine("IP: " + i);
+        }
+    }
+}
